4a6e7164c89aa787a32cb436f16a6d6db2e47b18
Matias Wasiak <matiaswasiak@gmail.com>
2022-09-29 16:23:38 -0300

    Escalando la arquitectura: Múltiples entidades

---
+++ components/user/network.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const response = require("../../network/response");
+const controller = require("./controller");
+const router = express.Router();
+
+router.get("/", function (req, res) {
+  controller
+    .listUsers()
+    .then((userList) => {
+      response.success(req, res, userList, 200);
+    })
+    .catch((e) => {
+      response.error(req, res, "Unexpected Error", 500, e);
+    });
+});
+
+router.post("/", function (req, res) {
+  controller
+    .addUser(req.body.name)
+    .then((data) => {
+      response.success(req, res, data, 201);
+    })
+    .catch((err) => {
+      response.error(req, res, "Intenal error", 500, err);
+    });
+});
+
+module.exports = router;
