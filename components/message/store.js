956052ede0456114ec6e242e73e526553685714e
Matias Wasiak <matiaswasiak@gmail.com>
2022-09-29 12:45:18 -0300

    MongoDB: Almacenar y leer datos

--- components/message/store.js
+++ components/message/store.js
@@ -1,11 +1,24 @@
-const list = [];
+const db = require("mongoose");
+const Model = require("./model");
+
+db.Promise = global.Promise;
+db.connect("mongodb+srv://root:root@cluster0.hau4sfc.mongodb.net/test", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  dbName: "Telegrom",
+});
+console.log("[db] Conectada con éxito");
 
 function addMessage(message) {
-  list.push(message);
+  // list.push(message);
+  const myMessage = new Model(message);
+  myMessage.save();
 }
 
-function getMessage() {
-  return list;
+async function getMessage() {
+  // return list;
+  const messages = await Model.find();
+  return messages;
 }
 
 module.exports = {
