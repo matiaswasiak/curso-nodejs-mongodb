661bd5ddd6ed753b263408988877a67c5396142c
Matias Wasiak <matiaswasiak@gmail.com>
2022-09-29 13:00:20 -0300

    MongoDB: Actualizar datos

--- components/message/store.js
+++ components/message/store.js
@@ -21,9 +21,20 @@ async function getMessage() {
   return messages;
 }
 
+async function updateText(id, message) {
+  const foundMessage = await Model.findOne({
+    _id: id,
+  });
+
+  foundMessage.message = message;
+  const newMessage = await foundMessage.save();
+  return newMessage;
+}
+
 module.exports = {
   add: addMessage,
   list: getMessage,
+  updateText,
   // get
   // update
   // delete
